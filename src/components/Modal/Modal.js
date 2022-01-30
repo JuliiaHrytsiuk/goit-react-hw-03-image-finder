e5c582126759386db0ac65ee2b5241cd1c1d84cc
JuliiaHrytsiuk <tuuspoliglotus@gmail.com>
2022-01-30 15:48:32 +0200

    Modal

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { createPortal } from "react-dom";
+import PropTypes from "prop-types";
+import { ModalContent, ModalBackdrop } from "./Modal.styled";
+// import { render } from "@testing-library/react";
+import { Component } from "react/cjs/react.production.min";
+
+const modalRoot = document.querySelector("#modal-root");
+
+class Modal extends Component {
+  componentDidMount() {
+    console.log("you");
+
+    window.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  handleKeyDown = (e) => {
+    if (e.code === "Escape") {
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = (event) => {
+    if (event.currentTarget === event.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <ModalBackdrop onClick={this.handleBackdropClick}>
+        <ModalContent>{this.props.children}</ModalContent>
+      </ModalBackdrop>,
+      modalRoot
+    );
+  }
+}
+
+// Modal.propTypes = {
+//   children: PropTypes.node,
+// };
+
+export default Modal;
